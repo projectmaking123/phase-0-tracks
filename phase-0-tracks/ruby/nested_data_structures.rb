685b681a7c861fb35b2282ceffd37f07febc85ca
projectmaking123 <jason.89.wang@gmail.com>
2017-01-15 16:28:56 -0500

    5.4

---
+++ phase-0-tracks/ruby/nested_data_structures.rb
@@ -0,0 +1,11 @@
+colors ={
+	white: ["Anti-flash white", "Antique white", "Beige"],
+	grey: ["Ash grey", "Battleship grey", "Blue-grey",	"Cadet grey"],
+	red: ["Alizarin crimson", "Amaranth", "American rose", "Auburn Burgundy"],
+	brown: ["Amber", "Auburn", "Beaver", "Beige", "Bistre"]
+}
+
+p colors[:white][0] #Anti-flash white
+p colors[:grey][1] #battleship grey
+p colors[:red][2] #american rose
+p colors[:brown][3] #beige 
