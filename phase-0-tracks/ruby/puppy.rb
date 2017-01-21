1d3fd4152658f662467d56b9a483284919b17078
projectmaking123 <jason.89.wang@gmail.com>
2017-01-21 15:04:45 -0500

    6.2

--- phase-0-tracks/ruby/puppy.rb
+++ phase-0-tracks/ruby/puppy.rb
@@ -1,8 +1,65 @@
+class Cat
+  cat1 = Cat.new
+  
+  def initialize
+    "Initializing new cat instance ..."
+  end
+  
+  def purr(num)
+    (num.to_i).times {
+      puts "purr"
+    }
+  end
+  
+  def sleep(hours)
+    puts "slept for #{hours} hours"
+  end
+  catarr = []
+  
+  50.times {
+    catarr << Cat.new
+  }
+ 
+  catarr.each { |instances|
+    instances.sleep("five")
+    instances.purr(2)
+  }
+  
+end
+
 class Puppy
 duchess = Puppy.new
 fido = Puppy.new
 spot = Puppy.new
 
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+  
+  def speak(num)
+    (num.to_i).times do 
+      puts "woof"  
+    end
+  end
+  
+  def rollover
+    puts "rollover"
+  end
+  
+  def dog_years(years)
+      years.to_i * 7
+  end
+  
+  def sit
+    puts "sit"
+  end
+  
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+  
+Puppy.new
 end
 
 # SPECIES ------------------------
@@ -10,8 +67,8 @@ end
 
 # CHARACTERISTICS ----------------
 # Size: varies
-# Friendly: varies
-# Noise level: varies
+# Friendly: yes
+# Eye count: 2
 # Name: varies
 # Color: varies
 
