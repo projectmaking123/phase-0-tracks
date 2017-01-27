5e0da9cdf1a8d3503a36a19b583e52ecbfb054ee
projectmaking123 <jason.89.wang@gmail.com>
2017-01-27 01:40:32 -0500

    final

--- phase-0-tracks/ruby/gps6/my_solution.rb
+++ phase-0-tracks/ruby/gps6/my_solution.rb
@@ -9,50 +9,58 @@
 require_relative 'state_data'
 
 class VirusPredictor
+  attr_reader :state_of_origin, :population, :population_density
 
+  #This method is introducng each instance variable to the class.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #returns predicted deaths and speed of spread results.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  #runs a conditional calculation based on the states popuation. Must draw state information from state_data.rb. Prints number of deaths.
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+
+    if population_density >= 200
+      rate = 0.4
+    elsif population_density >= 150
+      rate = 0.3
+    elsif population_density >= 100
+      rate = 0.2
+    elsif population_density >= 50
+      rate = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      rate =  0.05
     end
+    
+    number_of_deaths = (population * rate).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #accesses the hash in state data, depending which stte is accessed the density will determine how much the speed is increased by. prints the speed in months.
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
+    if population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
+    elsif population_density >= 150
       speed += 1
-    elsif @population_density >= 100
+    elsif population_density >= 100
       speed += 1.5
-    elsif @population_density >= 50
+    elsif population_density >= 50
       speed += 2
     else
       speed += 2.5
@@ -69,19 +77,20 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
-
+STATE_DATA.each do |k, v| 
+  state = VirusPredictor.new(k, STATE_DATA[k][:population_density], STATE_DATA[k][:population])
+    state.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# One uses a rocket equal to set the key as a string, the other uses : to set the key as a symbol
+# # What does require_relative do? How is it different from require?
+# require_relative allows accesses to the methods and function of another file, while require only allows for a read only data set.
+# # What are some ways to iterate through a hash?
+# hash.each or while loops
+# # When refactoring virus_effects, what stood out to you about the variables, if anything?
+# the variables were repeated often
+# # What concept did you most solidify in this challenge?
+# reducting repeated code
